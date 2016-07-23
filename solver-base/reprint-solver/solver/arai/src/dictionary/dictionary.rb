e3bd57653c9fd1a176abbc9841237a9823d04a3a
SandoBP13049 <bp13049@shibaura-it.ac.jp>
2016-07-24 02:20:44 +0900

    riprint-solver をコピー

---
+++ solver-base/reprint-solver/solver/arai/src/dictionary/dictionary.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+$KCODE="u"
+
+#
+# 単語辞書に関わるモジュール
+#
+module RS::Dictionary; end
+
+require 'dictionary/dictionary_database'
+require 'dictionary/katsuyo'
+require 'dictionary/word_converter'
+require 'dictionary/seed'
